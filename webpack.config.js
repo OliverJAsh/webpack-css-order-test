9abc296e2c30c3a0afead3d026b324552dd836e2
Oliver Joseph Ash <oliverjash@gmail.com>
2024-06-04 11:04:31 +0100

    Format

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: "./src/main.js",
-    plugins: [new MiniCssExtractPlugin()],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, {
-                    loader: "css-loader",
-                    options: {
-                        modules: {
-                            localIdentName: '[path][name]__[local]'
-                        }
-                    }
-                }],
+  mode: "production",
+  entry: "./src/main.js",
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[path][name]__[local]",
+              },
             },
+          },
         ],
-    },
+      },
+    ],
+  },
 };
